78f91263ae75e0ab0a3fd49d312bbdc58957d338
nimbus117 <mail@nimbus117.co.uk>
2018-10-19 13:49:27 +0100

    added IPlay and ISell interfaces

---
+++ src/main/java/interfaces/IPlay.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface IPlay {
+    String play();
+}
---
+++ src/main/java/interfaces/ISell.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface ISell {
+    double calculateMarkup();
+    double getSellPrice();
+}
